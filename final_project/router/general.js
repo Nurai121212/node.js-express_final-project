e896313a22a7f83b60264dc0f35bf9c1a9211e27
Nurai <nooraselfeght@gmail.com>
2024-09-24 22:00:34 +0600

    update general.js & auth_users.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesUserExist = (username) => {
+  const filteredUser = users.filter(elem => elem.username === username);
+
+  if(filteredUser.length){
+    return true
+  }else {
+    return false
+  }
+};
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(username && password){
+    if(!doesUserExist(username)){
+      users.push({username, password});
+      return res.status(200).json({message: 'User successfully registered'})
+    }else{
+      return res.status(404).json({message: 'User already exists'})
+    }
+  }else{
+    return res.status(404).json({message: 'Unable to register'})
+  }
 });
 
 // Get the book list available in the shop
@@ -69,5 +90,53 @@ public_users.get('/review/:isbn',function (req, res) {
     return res.status(404).json({message: "No book found with this ISBN"});
   }
 });
+
+// Get the list of books
+async function getAllBooks() {
+  try{
+    const res = await axios.get('http://localhost:5000/');
+    console.log(`Books list: ${JSON.stringify(res.data)}`);
+  }catch(error){
+    console.log(`Error ${error}`);
+  };
+};
+
+getAllBooks();
+
+// Get the book details based on its ISBN
+async function getBookByISBN() {
+  try{
+    const res = await axios.get('http://localhost:5000/isbn/2');
+    console.log(`Book with this ISBN: ${JSON.stringify(res.data)}`);
+  }catch(error){
+    console.log(`Error ${error}`);
+  };
+};
+
+getBookByISBN();
+
+// Get the book details based on author
+async function getBookByAuthor() {
+  try{
+    const res = await axios.get('http://localhost:5000/author/Unknown');
+    console.log(`Books by this author: ${JSON.stringify(res.data)}`);
+  }catch(error){
+    console.log(`Error ${error}`);
+  };
+};
+
+getBookByAuthor();
+
+// Get the book details based on title
+async function getBookByTitle() {
+  try{
+    const res = await axios.get('http://localhost:5000/title/the book of job');
+    console.log(`Book with this title: ${JSON.stringify(res.data)}`);
+  }catch(error){
+    console.log(`Error ${error}`);
+  };
+};
+
+getBookByTitle();
 
 module.exports.general = public_users;
